d16da009a1a1d3546442efeb8d5bfe9ecfedb834
Spiralyfox <dauriacmatteo@gmail.com>
2026-04-13 14:20:24 +0200

    2.3 Mission 3 : Introduction aux méthodes

--- Projet_Java_BTS_SIO_Methodes/src/Main.java
+++ Projet_Java_BTS_SIO_Methodes/src/Main.java
@@ -35,10 +35,15 @@ public class Main {
         // Voiture v1 = new Voiture("Peugeot", "208", 2021, 14500);
 
         Voiture v1 = new Voiture("Peugeot", "208", 2021, 14500);
+
+        /*
         System.out.println("Marque : " + v1.marque);
         System.out.println("Modèle : " + v1.modele);
         System.out.println("Année : " + v1.annee);
         System.out.println("Prix : " + v1.prix);
+         */
+
+        v1.afficherDescription ();
 
     }
 }
